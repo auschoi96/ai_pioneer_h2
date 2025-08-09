86d44b5ce026e6dd1368fecf6d40f36798789a60
auschoi96 <auschoi96>
2025-08-09 18:45:34 +0000

    VS to Agent Bricks
    remove VS instances and use Agent Bricks directly instead

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
 
 from typing import Any, Generator, Optional
-from databricks.sdk.service.dashboards import GenieAPI
 import mlflow
+import mlflow.deployments
 from databricks.sdk import WorkspaceClient
 from mlflow.entities import SpanType
 from mlflow.pyfunc.model import ChatAgent
@@ -13,14 +13,14 @@ from mlflow.types.agent import (
 import dspy
 import uuid
 import os
-from databricks.vector_search.client import VectorSearchClient
+
 
 mlflow.dspy.autolog()
 LLM_ENDPOINT_NAME = "databricks-llama-4-maverick"
 lm = dspy.LM(model=f"databricks/{LLM_ENDPOINT_NAME}")
 dspy.settings.configure(lm=lm)
-VECTOR_SEARCH_ENDPOINT_NAME = "ka-769ce120-vs-endpoint"
-vectorSearchIndexName = "__databricks_internal_catalog_tiles_2703287350484668.769ce120_73ad70ab11e841cc.ka_769ce120_709cf6e4_index"
+endpoint_name= "ka-01481fed-endpoint"
+endpoint_url = "https://e2-demo-field-eng.cloud.databricks.com/serving-endpoints/gemma3n_image_audio_text/invocations"
 
 class rag_signature(dspy.Signature):
   """
@@ -32,25 +32,23 @@ class rag_signature(dspy.Signature):
 class DSPyChatAgent(ChatAgent):     
     def __init__(self):
       self.rag_signature = rag_signature
-      self.vector_endpoint = VECTOR_SEARCH_ENDPOINT_NAME
-      self.vector_index = vectorSearchIndexName
+      self.endpoint_name = endpoint_name
       self.rag_agent = dspy.ReAct(self.rag_signature, tools=[self.sec_search],max_iters=1)
+    
 
     def sec_search(self, databricks_question):
         """This function needs the User's question. The question is used to pull documentation about Databricks. Use the information to answer the user's question"""
-
-        #TODO: Set this in the init
-        VECTOR_SEARCH_ENDPOINT_NAME = self.vector_endpoint
-        vectorSearchIndexName = self.vector_index
-
-        vsc = VectorSearchClient(
+        client = mlflow.deployments.get_deploy_client("databricks")
+        response = client.predict(
+            endpoint=self.endpoint_name,
+            inputs={"dataframe_split": {
+                "columns": ["input"],
+                "data": [[
+                    [{"role": "user", "content": databricks_question}]
+                ]]
+            }}
         )
-
-        index = vsc.get_index(endpoint_name=self.vector_endpoint, index_name=self.vector_index)
-
-        result = index.similarity_search(num_results=3, columns=["chunk_text"], query_text=databricks_question)
-
-        return result['result']['data_array'][0][0]
+        return response['predictions']['output'][0]['content'][0]['text']
       
     def prepare_message_history(self, messages: list[ChatAgentMessage]):
         history_entries = []
